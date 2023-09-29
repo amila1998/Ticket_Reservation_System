f34653014ba6cc44fb4a4e8d34a99dde2f33d035
amila1998 <amiladevin@gmail.com>
2023-09-29 16:49:05 +0530

    footer

--- trs_web_application/src/screens/Dashboard.js
+++ trs_web_application/src/screens/Dashboard.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div style={{ display: "flex", width: "100%" }}>
+      <div
+        // className="bg-secondary"
+        style={{
+          width: "20%",
+          height: "85vh",
+          backgroundColor: "rgb(35, 115, 252,0.5)",
+        }}
+      ></div>
+      <div style={{ width: "80%", marginLeft: "5px" }}>ajksdjkasdkasdka</div>
+    </div>
+  );
 };
 
 export default Dashboard;
