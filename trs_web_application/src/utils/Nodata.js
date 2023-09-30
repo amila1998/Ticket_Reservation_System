a4a0d0fe887a1e92a753ec7af75e37f87b441981
amila1998 <amiladevin@gmail.com>
2023-09-30 12:16:02 +0530

    user create

---
+++ trs_web_application/src/utils/Nodata.js
@@ -0,0 +1,12 @@
+import React from "react";
+import no_data_anim from "../assets/no_data.gif";
+
+const Nodata = () => {
+  return (
+    <div>
+      <img src={no_data_anim}></img>
+    </div>
+  );
+};
+
+export default Nodata;
