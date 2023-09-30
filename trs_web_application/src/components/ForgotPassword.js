1d5b16fab6caf854ef4c7a059b9dc33104c73b02
IT20259334 Aththanayaka H.A.M.U.I <it20259334@my.sliit.lk>
2023-09-30 12:20:12 +0530

    Forgot Password & Reset Password Frontend

--- trs_web_application/src/components/ForgotPassword.js
+++ trs_web_application/src/components/ForgotPassword.js
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ForgotPassword = () => {
-  return <div>ForgotPassword</div>;
+  const [nic, setNic] = useState("");
+  const [email, setEmail] = useState("");
+  const handleForgotPassword = async (e) => {
+    
+  };
+
+  return (
+    <>
+    <ToastContainer
+      position="top-left"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+    <div style={{ margin: "auto", alignItems: "center", padding: "5px" }}>
+      <div
+        style={{ margin: "auto", alignItems: "center", textAlign: "center" }}
+      >
+        <h2>Forgot Password</h2>
+      </div>
+      <form>
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">
+            NIC/Passport Number
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder="787141785V"
+            onChange={(e) => setNic(e.target.value)}
+          />
+        </div>
+        <label for="exampleFormControlInput2" class="form-label">
+          Email
+        </label>
+        <input
+          type="email"
+          id="exampleFormControlInput2"
+          class="form-control"
+          aria-describedby="passwordHelpBlock"
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <br />
+        <center>
+          <button onClick={handleForgotPassword} className="btn btn-primary">
+            Forgot Password
+          </button>
+        </center>
+      </form>
+    </div>
+  </>
+  )    
+ 
 };
 
 export default ForgotPassword;
